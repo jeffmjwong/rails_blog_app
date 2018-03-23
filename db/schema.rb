95d12fd875019fc7d641f700481362a6f2d3df80
Jeff Wong <jeffmjwong@gmail.com>
2018-03-23 21:10:41 +1000

    Add additional attributes to post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322110014) do
+ActiveRecord::Schema.define(version: 20180323095423) do
 
   create_table "lols", force: :cascade do |t|
     t.string "medium"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180322110014) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "body"
   end
 
 end
