14b9f7722c84db1c0c30c61cb9d05365c1ad7e25
Jeff Wong <jeffmjwong@gmail.com>
2018-03-24 13:35:30 +1000

    Add Page model and Pages controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180323095423) do
+ActiveRecord::Schema.define(version: 20180324033345) do
 
   create_table "lols", force: :cascade do |t|
     t.string "medium"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
