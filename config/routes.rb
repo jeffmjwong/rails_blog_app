279d03c4e5e8fe986ad87be48ea0d1a5f44976e6
Jeff Wong <jeffmjwong@gmail.com>
2018-03-25 20:39:28 +1000

    Create show method in PagesController

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :lols
   resources :posts
   get '/pages', to: 'pages#index'
+  get '/pages/:id', to: 'pages#show', as: 'page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
