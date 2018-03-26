97eb02c3a40c1be0991e3516e5672b82ae8cdd4c
Jeff Wong <jeffmjwong@gmail.com>
2018-03-26 21:34:04 +1000

    Add edit, update and delete action

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :lols
   resources :posts
-  get '/pages', to: 'pages#index'
-  post '/pages', to: 'pages#create'
-  get '/pages/new', to: 'pages#new', as: 'new_page'
-  get '/pages/:id', to: 'pages#show', as: 'page'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :pages
+  # get '/pages', to: 'pages#index'
+  # post '/pages', to: 'pages#create'
+  # get '/pages/new', to: 'pages#new', as: 'new_page'
+  # get '/pages/:id', to: 'pages#show', as: 'page'
+  # get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
+  # patch '/pages/:id', to: 'pages#update'
+  # delete '/pages/:id', to: 'pages#destroy'
 end
